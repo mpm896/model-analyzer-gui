3d9166dff3d543ba53f1737f58a9314860e0b6ca
Matthew Martinez <mpm896@pennmedicine.upenn.edu>
2023-08-29 16:01:48 -0400

    Added ability to create dataframes

--- model-analyzer.py
+++ model-analyzer.py
@@ -6,7 +6,10 @@ Main script for the IMOD model analyzer GUI
 
 import PySimpleGUI as sg
 import os
-import scripts.utils as util
+import scripts.utils as u
+import scripts.setup as setup
+import scripts.analyze as a
+
 
 # Create the window layout. Just one column might be necessary
 
@@ -19,35 +22,51 @@ file_selection_column = [
         sg.Text("Select a folder:"),
         sg.In(size=(25,1), enable_events=True, key="-FOLDER-"),
         sg.FolderBrowse(button_text='Browse for parent folder', initial_folder='.')
-    ],
-    [
-        sg.HSeparator()
     ]
 ]
 
 file_list_column = [
     [
-        sg.Listbox(values=[], enable_events=True, size=(40,20), key="-FILE LIST-", horizontal_scroll=True)
+        sg.Listbox(values=[], enable_events=True, size=(40,20), key="-FILE LIST-", horizontal_scroll=True),
+        sg.Button(button_text='Make Dataframe', size=(9,2))
+    ],
+    [
+        sg.Button('Remove')
+    ]
+]
+
+dataframe_list_column = [
+    [
+        sg.Text("Dataframes", font=(None, 15, 'underline'))
+    ],
+    [
+        sg.Listbox(values=[], enable_events=True, size=(30,15), key="-DATAFRAME LIST-", horizontal_scroll=True)
     ]
 ]
 
 # ------- FULL LAYOUT ------- #
 
 layout = [
+
     [
-        sg.Column(file_selection_column)
+        sg.Push(), sg.Column(file_selection_column), sg.Push()
     ],
     [
-        sg.Push(), sg.Column(file_list_column, element_justification='c'), sg.Push()
+        sg.HSeparator()
     ],
     [
-        sg.Push(), sg.Button('Remove'), sg.Push()
+        sg.Column(file_list_column, element_justification='c'),
+        sg.VSeparator(),
+        sg.VPush(), sg.Column(dataframe_list_column, element_justification='c'), sg.VPush()
     ]
 ]
 
 # Create the window and bind keys
 window = sg.Window(title="IMOD Model Analyzer", layout=layout, finalize=True)
 window["-MODEL NAME-"].bind("<Return>", "_Enter") # The keybind must be done after the window initialization is finalized
+
+# Initialize the dataframes
+df_list = []
 
 # Event loop
 while True:
@@ -62,7 +81,7 @@ while True:
     elif event == "-MODEL NAME-" + "_Enter":
         # Get the model files from the given folder
         try:
-            file_list = util.read_models(folder, model_name)
+            file_list = u.read_models(folder, model_name)
         except:
             file_list = []
         window["-FILE LIST-"].update(file_list)
@@ -79,21 +98,28 @@ while True:
 
         # Get the model files from the given folder
         try:
-            file_list = util.read_models(folder, model_name)
+            file_list = u.read_models(folder, model_name)
         except:
             file_list = []
         window["-FILE LIST-"].update(file_list)
 
+
     # Event for removing a model from the list
     if event == 'Remove':
-        util.remove_model(file_list, values)
+        u.remove_model(file_list, values)
         window["-FILE LIST-"].update(file_list)
+        
+    # Display Dataframe names in the dataframe box when hit the "make dataframe" button
+    if event == 'Make Dataframe':
+        try:
+            if setup.makeDataframe(file_list) not in df_list:
+                df_list.append(setup.makeDataframe(file_list))
+        except NameError:
+            pass
         
             
     if event == "-FILE LIST-":
         pass
-
-
 
 
 window.close()
---
+++ scripts/setup.py
@@ -0,0 +1,91 @@
+'''
+Setup functions for the IMOD models into a Pandas dataframe.
+
+@author: Matt Martinez
+'''
+
+import subprocess
+import os.path
+import re
+import pandas as pd
+
+def model2point(file_list: list) -> list:
+    
+    '''
+    Utilize the PEET command, model2point, to convert
+    desired model files into xyz point files. Store those files
+    in a temporary directory
+
+    Return list for the dataframe
+
+    Args: file_list - list of file names
+    '''
+    new_file_list = []
+    for file in file_list:
+        file_prefix = file.split('/')[:-1]
+        file_prefix.pop(0)
+
+        # Create new file path and name
+        name = ''
+        for item in file_prefix:
+            name += item + '_'
+
+        file_path = '../__model_cache__/'
+        file_name = name + file.split('/')[-1].split('.')[0] + '.txt' 
+
+        # Run model2point on the models, if the text files don't already exit
+        if not os.path.isfile(f'{file_path}{file_name}'):
+            process = subprocess.run(['model2point', '-ob', f'{file}', f'{file_path}{file_name}'])
+        
+        new_file_list.append(f'{file_path}{file_name}')
+
+    return new_file_list
+
+def makeDataframe(file_list: list) -> dict:
+
+    '''
+    From the IMOD models converted to text files, create
+    a Pandas dataframe for viewing
+
+    Return the dataframe
+
+    Args: file_list - list of .txt files
+    '''
+
+    models_dataFrame = dict()
+
+    # Read all text files into a list
+    files = model2point(file_list)
+
+    # Read in data points from the files and assemble into dataframe
+    for file in files:
+        with open(file, 'r') as f:
+            points = f.readlines()
+        points = [line.strip().split() for line in points]
+
+        #Convert points into a Pandas dataframe
+        df = pd.DataFrame({'Ob':[int(points[0][0])], 'Ct':[int(points[0][1])],
+                           'X':[float(points[0][2])], 'Y':[float(points[0][3])], 'Z':[float(points[0][4])]})
+        count = 1 
+        for line in points[1:]:
+            df = df.append(pd.DataFrame({'Ob':[int(points[count][0])], 'Ct':[int(points[count][1])],
+                                         'X':[float(points[count][2])], 'Y':[float(points[count][3])], 'Z':[float(points[count][4])]}),
+                                         ignore_index=True)
+            count += 1
+
+        models_dataFrame[file.split('/')[-1]] = df
+    return models_dataFrame
+
+
+
+def test_model2point():
+    model2point(['/ChangLab1-hd3/matt/GUI_IMOD_models/test_models/MV_model.mod'])
+
+def test_makeDateframe():
+    makeDataframe(['/ChangLab1-hd3/matt/GUI_IMOD_models/test_models/MV_model.mod',
+                   '/ChangLab1-hd3/matt/GUI_IMOD_models/test_models/MV_model2.mod',
+                   '/ChangLab1-hd3/matt/GUI_IMOD_models/test_models/MV_model3.mod',
+                   '/ChangLab1-hd3/matt/GUI_IMOD_models/test_models/MV_model4.mod'])
+
+if __name__ == '__main__':
+    test_makeDateframe()
